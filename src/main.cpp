317f1e6927c795a75ffd68464f99e5b9d4a3204e
Magrit <mikael.pettersson201@gmail.com>
2017-09-02 20:03:13 +0200

    Fix for CMakeLists.txt or something

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <GLFW\glfw3.h>
+#include <iostream>
 int main(int argv, char** argc)
 {
-
+	if (!glfwInit()) {
+		std::cout << "EVERYTHING IS HORRIBLE!" << std::endl;
+		return -1;
+	}
     return 1;
 }
