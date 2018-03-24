670bbdc5121ca128e9b05b7e378617aa34c1bc71
Magrit <mikael.pettersson201@gmail.com>
2018-03-24 01:32:22 +0100

    Added vulkan libs and headers. Added some init funcs for vulkan instance Everyhing is super serious!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,81 @@
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW\glfw3.h>
 #include <iostream>
+#include <vector>
+
+
+const int WIDTH = 1024;
+const int HEIGHT = 768;
+
+GLFWwindow* 
+initWindow()
+{	
+  if (!glfwInit()) {
+		std::cout << "EVERYTHING IS HORRIBLE! GLFW WONT START!" << std::endl;
+		exit(-1);
+	}
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  return glfwCreateWindow(1024, 768, "Serious vulkan window", nullptr, nullptr);
+}
+
+void 
+mainLoop(GLFWwindow* window)
+{
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+}
+void
+createVulkanInstance()
+{
+  VkApplicationInfo appInfo;
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Serious vulkan";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "Serious engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+  uint32_t glfwExtensionCount = 0;
+  const char** glfwExtensions;
+
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  createInfo.enabledExtensionCount = glfwExtensionCount;
+  createInfo.ppEnabledExtensionNames = glfwExtensions;
+  createInfo.enabledLayerCount = 0;
+
+  VkInstance instance;
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create instance!");
+  }
+  else {
+    std::cout << "Vulkan instance created successfully!" << std::endl;
+  }
+}
+
+
 int main(int argv, char** argc)
 {
-	if (!glfwInit()) {
-		std::cout << "EVERYTHING IS HORRIBLE!" << std::endl;
-		return -1;
-	}
-    return 1;
+  auto window = initWindow();
+  createVulkanInstance();
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  std::vector<VkExtensionProperties> extensions(extensionCount);
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+  std::cout << "available extensions:" << std::endl;
+
+  for (const auto& extension : extensions) {
+    std::cout << "\t" << extension.extensionName << std::endl;
+  }
+
+  mainLoop(window);
+  return 1;
 }
